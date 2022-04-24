b8550b9221fa46474387664cbf17adc3f20196a0
Rhythm Saha <rs.2001.saha@gmail.com>
2022-04-24 13:14:28 +0530

    authContext

---
+++ context/authContext.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const authContext = createContext({
+    isLoggedIn: false,
+    authLoading: true,
+    fullName: null,
+    authToken: null,
+    email: null,
+    login: () => {},
+    logout: () => {},
+});
+
+export default function AuthProvider({ children }) {
+    const [user, setUser] = useState({
+        isLoggedIn: false,
+        authLoading: true,
+        fullName: null,
+        email: null,
+        authToken: null,
+    });
+    const [authToken, setAuthToken] = useState(null);
+
+    useEffect(() => {
+        const token = window.localStorage.getItem("authToken");
+        if (!token) return setUser((prev) => ({ ...prev, authLoading: false }));
+
+        axios
+            .post(`${process.env.NEXT_PUBLIC_API_URL}/auth/authorize`, {
+                JWT_TOKEN: token,
+            })
+            .then((response) => {
+                setUser({
+                    isLoggedIn: true,
+                    authLoading: false,
+                    fullName: response.data?.fullName,
+                    email: response.data?.email,
+                    authToken: token,
+                });
+            })
+            .catch((err) => {
+                window.localStorage.removeItem("authToken");
+                setUser((prev) => ({
+                    ...prev,
+                    authLoading: false,
+                    isLoggedIn: false,
+                }));
+            });
+    }, [authToken]);
+
+    const login = (token) => {
+        window.localStorage.setItem("authToken", token);
+        setAuthToken(token);
+    };
+    const logout = () => {
+        window.localStorage.removeItem("authToken");
+        setUser({
+            isLoggedIn: false,
+            authLoading: true,
+            fullName: null,
+            email: null,
+            authToken: null,
+        });
+        setAuthToken(null);
+    };
+
+    return (
+        <authContext.Provider value={{ ...user, login, logout }}>
+            {children}
+        </authContext.Provider>
+    );
+}
+
+export const useAuthCtx = () => {
+    const authCTX = useContext(authContext);
+    return authCTX;
+};
