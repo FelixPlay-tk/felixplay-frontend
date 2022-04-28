2db333c99d5f3c584a7a9c8b1d365f7f68802b2c
Rhythm Saha <rs.2001.saha@gmail.com>
2022-04-28 19:26:58 +0530

    test api limitations

--- pages/api/all.js
+++ pages/api/all.js
@@ -1,17 +1,13 @@
 import { connect, connection } from "mongoose";
 
-import crypto from "crypto";
 import movieModel from "../../models/movieModel";
-
-// import movieSchema from "../../models/movieModel";
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
     await connect(process.env.DB_URL);
 
     try {
-        const movies = await movieModel.find().limit(100);
+        const movies = await movieModel.find();
+        connection.close();
         res.status(200).json(movies);
     } catch (error) {
         res.send("error");
